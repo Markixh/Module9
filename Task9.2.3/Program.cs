122600612c53f80bd98e03d5b11fba930e6f29c0
Марков Андрей Анатольевич <andrejmarko@yandex.ru>
2022-10-29 22:38:52 +0300

    Добавлено задание 9.2.3

---
+++ Task9.2.3/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Task9_2_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            catch (Exception ex) when (ex is RankException)
+            {
+                Console.WriteLine("Возникло исключение RankException");
+                Console.WriteLine(ex.GetType());
+            }
+            finally
+            {
+                Console.WriteLine("Сработал finally");
+            }
+            Console.ReadLine();
+        }
+    }
+}
