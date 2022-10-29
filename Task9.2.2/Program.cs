be8a28747aa8b1d80d51cd454771d7a5116f749a
Марков Андрей Анатольевич <andrejmarko@yandex.ru>
2022-10-29 22:35:13 +0300

    Добавление задание 9.2.2

---
+++ Task9.2.2/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Task9_2_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            catch(Exception ex) when (ex is ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Возникло исключение ArgumentOutOfRangeException");
+            }
+            finally
+            {
+                Console.WriteLine("Сработал finally");
+            }
+            Console.ReadLine();
+        }
+    }
+}
