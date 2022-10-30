49f45d8497181d1feb0b5438e350e332eb682dc6
Марков Андрей Анатольевич <andrejmarko@yandex.ru>
2022-10-31 00:30:40 +0300

    Добавлено задание 9.3.13

---
+++ Task9.3.13/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Task9_3_13
+{
+    internal class Program
+    {
+        delegate int RandomNumberDelegate();
+
+        static void Main(string[] args)
+        {
+            RandomNumberDelegate randomNumberDelegate = delegate()
+            {
+                return new Random().Next(0, 100);
+            };
+            int result = randomNumberDelegate.Invoke();
+            Console.WriteLine(result);
+            Console.Read();
+        }
+    }
+}
