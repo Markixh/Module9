a09892a551370dc4f9a76ef3d161236913b51d3c
Марков Андрей Анатольевич <andrejmarko@yandex.ru>
2022-10-30 23:42:35 +0300

    Добавлено задание 9.3.4

---
+++ Task9.3.4/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Task9_3_4
+{
+    internal class Program
+    {
+        delegate void Func(int a, int b);
+
+        static void Main(string[] args)
+        {
+            Func func = Sub;
+            func += Sum;
+
+            func(5, 10);
+            Console.ReadLine();
+        }
+
+        static void Sub(int a, int b)
+        {
+            Console.WriteLine($"{b} - {a} = {b - a}");
+        }
+
+        static void Sum(int a, int b)
+        {
+            Console.WriteLine($"{a} + {b} = {a + b}");
+        }
+    }
+}
