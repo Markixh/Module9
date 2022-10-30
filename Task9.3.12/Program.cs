93e0500098383ba9e7ee8f94289661885c622a20
Марков Андрей Анатольевич <andrejmarko@yandex.ru>
2022-10-31 00:26:45 +0300

    Добавлено задание 9.3.12

---
+++ Task9.3.12/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Task9_3_12
+{
+    internal class Program
+    {
+        delegate void ShowMessageDelegate(string _message);
+
+        static void Main(string[] args)
+        {
+            ShowMessageDelegate showMessageDelegate = delegate (string _message)
+            {
+                Console.WriteLine(_message);
+            };
+            showMessageDelegate.Invoke("Hello World!");
+            Console.Read();
+        }
+    }
+}
