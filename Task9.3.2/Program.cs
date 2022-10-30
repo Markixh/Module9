54f5db161fd4867cbc91fdd479a8f035ef16e151
Марков Андрей Анатольевич <andrejmarko@yandex.ru>
2022-10-30 23:28:54 +0300

    Добавлено задание 9.3.2

---
+++ Task9.3.2/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Task9_3_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func func = Sub;
+            Console.WriteLine($"10 - 5 = {Sub(5,10)}");
+            Console.ReadLine();
+        }
+
+        public delegate int Func(int a, int b);
+
+        static int Sub(int a, int b)
+        {
+            return b - a;
+        }
+    }
+}
