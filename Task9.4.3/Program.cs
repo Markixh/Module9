42b790d6d6078576ac2fdaed30ce5a0f8a8b7161
Марков Андрей Анатольевич <andrejmarko@yandex.ru>
2022-10-31 21:24:36 +0300

    Добавлено задание 9.4.3

---
+++ Task9.4.3/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Task9_4_3
+{
+    internal class Program
+    {
+        delegate void childdelegate(Child child);
+
+        static void Main(string[] args)
+        {
+            childdelegate cd;
+            cd = parent;
+            cd(new Child());
+        }
+        static void parent(Parent parent)
+        {
+            Console.WriteLine(parent);
+        }
+    }
+
+    class Parent { }
+    class Child : Parent { }
+}
