506e3f192a850decbfba75f4041ad450ffc85fd1
Марков Андрей Анатольевич <andrejmarko@yandex.ru>
2022-10-31 21:08:55 +0300

    Добавлено задание 9.4.2

---
+++ Task9.4.2/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Task9_4_2
+{
+    internal class Program
+    {
+        delegate Car CarDelegate(string model);
+
+        static void Main(string[] args)
+        {
+            CarDelegate carDelegate;
+            carDelegate = BuildLexus;
+            Car c = carDelegate("Lexus");
+            Console.WriteLine(c);
+            Console.ReadLine();
+        }
+
+        static Car BuildLexus(string model)
+        {
+            return new Lexus();
+        }
+    }
+    class Car { }
+    class Lexus : Car { }
+}
